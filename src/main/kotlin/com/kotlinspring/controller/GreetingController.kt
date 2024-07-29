2501f3cd96aadbf9e0073ff6f55be4b5b24b03c6
Fabrice <fabrice.design85@gmail.com>
2024-07-29 21:48:58 +0200

    add logger and prod config

--- src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.kotlinspring.controller
 
 import com.kotlinspring.service.GreetingsService
+import mu.KLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/v1/greetings")
 class GreetingController(val greetingsService: GreetingsService) {
 
+    // ajout du Logguer
+    companion object : KLogging()
+
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name : String): String{
+        logger.info("Name is $name")
         return greetingsService.retrieveGreeting(name)
     }
 }
--- src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
+++ src/main/kotlin/com/kotlinspring/service/GreetingsService.kt
@@ -1,9 +1,12 @@
 package com.kotlinspring.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsService {
+    @Value("\${message}")
+    lateinit var message: String
 
-    fun retrieveGreeting(name: String) = "Hello $name"
+    fun retrieveGreeting(name: String) = "$name, $message"
 }
